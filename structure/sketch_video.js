db87c38831c05dd475eb955ec7d3cff4cd60c124
kazufumiw <Kazufumi.Watanabe1@ibm.com>
2019-05-08 22:15:08 +0900

    add mode

--- structure/sketch_video.js
+++ structure/sketch_video.js
@@ -1,6 +1,6 @@
 var capture;
-var nx = 48;
-var ny = 36;
+var nx = 90;
+var ny = 60;
 var tile;
 var index;
 var colour;
@@ -9,6 +9,7 @@ var trans;
 var mode = 0;
 var count = 0;
 var framecount = 0;
+var alphabet = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
 
 function preload(){
 	capture = createCapture(VIDEO);
@@ -45,6 +46,7 @@ function draw() {
       greyscale = round(red(colour)*0.222+green(colour)*0.707+blue(colour)*0.071);
       rectMode(CORNER);
       if(mode == 0){
+				noStroke();
         ellipse(tile*i+trans, tile*(j), tile*greyscale/150.0*4*(mouseX/windowWidth-0.5), tile*greyscale/150.0*4*(mouseY/windowHeight-0.5));
         fill(red(colour), green(colour), blue(colour), 150);
       }
@@ -73,6 +75,22 @@ function draw() {
         colorMode(HSB, 360, 100, 100, 100);
         fill(0,0,100);
       }
+			if(mode == 6){
+				stroke(0,255,0);
+				if(Math.random() < 0.05){
+					fill(255,255,255);
+				}
+				else{
+					fill(0,255,0);
+				}
+				textAlign(CENTER);
+				textSize(14);
+				if(red(colour)+green(colour)+blue(colour) < 255*0.5*3){
+					if(Math.random() < 0.5){
+						text(alphabet[int(Math.random()*25)], tile*i+trans, tile*(j));
+					}
+				}
+      }
     }
   }
 }
@@ -83,6 +101,6 @@ function windowResized() {
 
 function mouseClicked(){
   count = count + 1;
-  mode = count%6;
+  mode = count%7;
   console.log(count, mode);
 }
