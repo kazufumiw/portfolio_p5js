7bd08b4b14f9546f05facd24ec91085661bfe4e9
kazufumiw <Kazufumi.Watanabe1@ibm.com>
2019-07-07 16:47:50 +0900

    add images

--- sinedfermale/sketch_video.js
+++ sinedfermale/sketch_video.js
@@ -1,24 +1,42 @@
 var img;
-var nx = 10*1;
-var ny = 18*1;
+var oimg;
+var nx = 20;
+var ny = 36;
 var tile;
-var index;
 var colour;
 var greyscale;
 var trans;
-var array = [];
 var sw = 200.0/nx;
 
 function preload(){
-	img = loadImage('fermale.jpg');
+	oimg = loadImage('fermale.jpg');
 }
 
 function setup() {
 	noLoop();
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, SVG);
 	background(0);
   colorMode(RGB);
-  frameRate(24);
+  frameRate(1);
+  //img.resize(nx, ny);
+  //img.loadPixels();
+	/*
+  if(windowWidth/nx < windowHeight/ny){
+    tile = windowWidth/nx;
+	}else{
+		tile = windowHeight/ny;
+	}
+  trans = (windowWidth-nx*tile)*0.5;
+	*/
+}
+
+function draw() {
+	background(0);
+	img = Object.assign(oimg);
+	//img = oimg.concat();
+	nx = 20*1.5*0.5;//nx+1;//*frameCount/100+5;
+	ny = 36*1.5*0.5;//ny+1.8;//*frameCount/100+9;
+	sw = 200.0/nx*1.3;
   img.resize(nx, ny);
   img.loadPixels();
   if(windowWidth/nx < windowHeight/ny){
@@ -27,30 +45,30 @@ function setup() {
 		tile = windowHeight/ny;
 	}
   trans = (windowWidth-nx*tile)*0.5;
-  console.log(nx, ny, tile, trans);
-}
 
-function draw() {
-  background(0);
+  //background(0);
   for(var i = 0; i < ny; i++){
-		array = [];
     for(var j = 0; j < nx; j++){
       colour = color(img.get(j, i));
 			clr = color(img.get(j+1, i));
 			greyscl = round(red(clr)*0.222+green(clr)*0.707+blue(clr)*0.071);
       greyscale = round(red(colour)*0.222+green(colour)*0.707+blue(colour)*0.071);
-			if(greyscale > 50){
+			if(greyscale > 70){
 				strokeWeight(sw);
 				stroke(red(colour), green(colour), blue(colour));
 				line(trans + tile*j, tile*i, trans + tile*j + tile, tile*i+tile);
 			}else{
-				strokeWeight(sw*0.5);
+				strokeWeight(sw*0.1);
 				stroke(red(colour), green(colour), blue(colour));
 				line(trans + tile*j, tile*i+tile, trans + tile*j + tile, tile*i);
 			}
     }
   }
+	//save("mySVG.svg");      // give file name
+	//print ("saved svg");
 }
+
+
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
